776713d217ad94230d74261b49d0aa4c6eeef690
jaja13090 <69601407+jaja13090@users.noreply.github.com>
2020-08-18 00:55:27 +0200

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -41,7 +41,34 @@ if choice == 2:
 if choice == 3:
     time.sleep(4)
     print("\033[1;94mNumbers 3 Selected")
-    print " "
-    execfile("PhoneNumber.py")
-else:
-    print("Error")
+    import re
+import json
+from urllib2 import urlopen
+
+
+phone_number = raw_input("Enter Phone Number:  ")
+
+key = "22157d91a21b4bea9804521fda0fe886"
+
+url = "http://apilayer.net/api/validate?access_key=" + key + "&number=" + phone_number + "&country_code=FR&format=1"
+
+response = urlopen(url)
+data = json.load(response)
+
+if ['valid'] == True:
+    print("Phone Number valid")
+if ['valid'] == False:
+    print("Invalid Phone Number")
+
+valid=data['valid']
+
+country_name=data['country_name']
+
+country_code=data['country_code']
+
+line_type=data['line_type']
+
+carrier=data['carrier']
+
+location=data['location']
+
